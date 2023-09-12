aea119644916d826fa934b35f2ba47299fd7e413
m-riss <95452643+m-riss@users.noreply.github.com>
2023-09-12 19:37:54 -0400

    Create Strings.cpp

---
+++ Strings.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <bits/stdc++.h>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+string First_Character_of_First_Seed = "";
+string Comma = "";
+string Space = "";
+string Colon = "";
+string Period = "";
+string QuestionMark = "";
+string First_Seed = "";
+string Second_Seed = "";
+string Third_Seed = "";
+string Fourth_Seed = "";
+string Fifth_Seed = "";
+string Sixth_Seed = "";
+string Seventh_Seed = "";
