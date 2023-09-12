9d9a861ce500cdffc1c36117f1a7989dd0931893
m-riss <95452643+m-riss@users.noreply.github.com>
2023-09-12 19:37:20 -0400

    Create Integers.cpp

---
+++ Integers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <bits/stdc++.h>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int First_Seed_Number = 0;
+int Exit_Variable = 0;
+int Seed_String_Length = 0;
+int Seed_String_Length_2 = 0;
+int i = 0;
+int First_Seed_Length = 0;
